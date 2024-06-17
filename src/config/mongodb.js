63004722082cd91f88d5e5b51d577d1d2f265764
ToitenDang <tkdang21@gmail.com>
2024-06-17 12:35:27 +0700

    Connect Database

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -2,4 +2,37 @@
  * Updated by trungquandev.com's author on August 17 2023
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
- */
+*/
+
+const MONGODB_URI = 'mongodb+srv://tkdang21:yNKvzshKo8CJTBL8@cluster0-dang.b3xc2q8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0-Dang'
+const DATABASE_NAME = 'trello-dang'
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+// Khởi tạo một đối tượng trelloDatabaseInstance là null (vì chưa kết nối)
+let trelloDatabaseInstance = null
+
+// Khởi tạo một đối tượng mongoClientInstance để connect tới MongoDB
+const mongoClientInstance = new MongoClient(MONGODB_URI, {
+  // Lưu ý: ServerApi có từ phiên bản MongoDB 5.0.0 trở lên, có thể không cần dùng nó
+  //        còn nếu dùng nó chúng ta cần chỉ định một cái Stable API Version của MongoDB
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+export const CONNECT_DB = async() => {
+  // Gọi kết nối đến MongoDB aslas URI đã khai báo trong thân của mongoClientInstance
+  await mongoClientInstance.connect()
+
+  // Kết nối thành công thì lấy database theo tên và gán ngược nó lại vào biến trelloDatabaseInstance ở trên
+  trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME)
+}
+
+// Function GET_DB này có nhiệm vụ export ra cái Trello Database Instance sau khi đã connect thành công tới MongoDB để sử dụng ở nhiều nơi trong code
+// Phải đảmbaor chỉ luôn gọi cái getDB này sau khi kết nối thành công tới Database
+export const GET_DB = () =>{
+  if (!trelloDatabaseInstance) throw new Error ('Must connect to Database first!')
+  return trelloDatabaseInstance
+}
--- src/server.js
+++ src/server.js
@@ -1,27 +1,43 @@
+/* eslint-disable no-console */
 
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
-
+import { CONNECT_DB, GET_DB } from '~/config/mongodb'
 const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+const START_SERVER = () => {
+  const hostname = 'localhost'
+  const port = 8017
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [{ id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' }],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.get('/', async(req, res) => {
+    console.log(await GET_DB().listCollections().toArray())
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello, I am running at http://${ hostname }:${ port }/`)
-})
+  app.listen(port, hostname, () => {
+    console.log (`Hello, I am running at http://${ hostname }:${ port }/`)
+  })
+}
+
+(async () => {
+  try {
+    console.log('1. Connecting to MongoDB Cloud Atlast!')
+    await CONNECT_DB()
+    console.log('2. Connected to MongoDB Cloud Atlast!')
+
+    // Khởi động Server sau khi kết nối Database thành công
+    START_SERVER()
+  } catch (error) {
+    console.error(error)
+    process.exit(0)
+  }
+})()
+
+//Chỉ khi kết nối tới Database thành công thì mới Start Server Back-end lên.
+// CONNECT_DB()
+//   .then(() => console.log('Connected to MongoDB Cloud Atlast!'))
+//   .then(() => START_SERVER())
+//   .catch(error => {
+//     console.error(error)
+//     process.exit(0)
+//   })
+

4a433b8afe20bdb710e84b32fa1eebd83aabf5fe
ToitenDang <tkdang21@gmail.com>
2024-06-20 10:13:56 +0700

    Express Routers

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,3 +3,19 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+
+import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { boardRouters } from './boardRoutes'
+
+const Router = express.Router()
+
+// Check APIs v1 status
+Router.get('/status', (req, res) => {
+  // Ở đây status là một method trong respon để trả về một mã code
+  res.status(StatusCodes.OK).json({ message: 'APIs V1 are ready to use.' })
+})
+
+// Board APIs
+Router.use('/boards', boardRouters)
+export const APIs_V1 = Router

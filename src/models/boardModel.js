1fc141b487c722b36d83a4daf23a6bb626f47ff6
ToitenDang <tkdang21@gmail.com>
2024-06-28 09:05:00 +0700

    Create Board Api

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -5,6 +5,9 @@
  */
 
 import Joi from 'joi'
+import { ObjectId } from 'mongodb'
+import { GET_DB } from '~/config/mongodb'
+import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
 
 // Define Collection ( name & schema)
 
@@ -14,14 +17,40 @@ const BOARD_COLLECTION_SCHEMA = Joi.object({
   slug: Joi.string().required().min(3).trim().strict(),
   description: Joi.string().required().min(3).max(256).trim().strict(),
 
-  columnOrderIds: Joi.array().items(Joi.string()).default([]),
+  columnOrderIds: Joi.array().items(
+    Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)
+  ).default([]),
 
   createdAt: Joi.date().timestamp('javascript').default(Date.now),
   updatedAt: Joi.date().timestamp('javascript').default(null),
   _destroy: Joi.boolean().default(false)// Khi người dùng xóa board ta chỉ gán cho nó là true và ẩn ở giao diện người dùng chứ không xóa hoàn toàn
 })
 
+const validateBeforeCreate = async (data) => {
+  return await BOARD_COLLECTION_SCHEMA.validateAsync(data, { abortEarly: false })
+}
+
+const createNew = async (data) => {
+  try {
+    const validData = await validateBeforeCreate(data)
+    // const createdBoard = await GET_DB().collection(BOARD_COLLECTION_NAME).insertOne(data)
+    // return createdBoard
+    // Có thể trả thẳng về trực tiếp không cần tạo biến
+    return await GET_DB().collection(BOARD_COLLECTION_NAME).insertOne(validData)
+  } catch (error) {throw new Error(error)}
+}
+
+const findOneById = async (id) => {
+  try {
+    console.log(id)
+    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({ _id: new ObjectId(id) })
+    return result
+  } catch (error) {throw new Error(error)}
+}
+
 export const boardModel = {
   BOARD_COLLECTION_NAME,
-  BOARD_COLLECTION_SCHEMA
+  BOARD_COLLECTION_SCHEMA,
+  createNew,
+  findOneById
 }

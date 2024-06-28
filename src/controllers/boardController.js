19412072942e769849d33fd627c58f89abb2020f
ToitenDang <tkdang21@gmail.com>
2024-06-28 11:10:46 +0700

    Get board

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -10,11 +10,24 @@ const createNew = async(req, res, next) => {
   try {
     // console.log(req.body)
 
+    // Điều hướng sang service
     const createBoard = await boardService.createNew(req.body)
+
+    // Có kết quả trả về phía client
     res.status(StatusCodes.CREATED).json(createBoard)
   } catch (error) { next(error) }
 }
 
+const getDetails = async(req, res, next) => {
+  try {
+    const boardId = req.params.id
+    const board = await boardService.getDetails(boardId)
+
+    res.status(StatusCodes.OK).json(board)
+  } catch (error) { next(error) }
+}
+
 export const boardController = {
-  createNew
+  createNew,
+  getDetails
 }

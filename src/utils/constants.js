58ad46658f4f5899c4da97566a7965e4bba79db0
ToitenDang <tkdang21@gmail.com>
2024-06-29 09:12:02 +0700

    Config Cors

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,9 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+
+// Những domain được phép truy cập tài nguyên của server
+export const WHITELIST_DOMAINS = [
+  'http://localhost:5173'
+  //...v.v ví dụ sau này deploy lên domain chính thức
+]
